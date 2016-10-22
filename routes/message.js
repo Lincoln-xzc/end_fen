c48a8e09e8f4d5c83ab5d1c3b8622e04efbb7641
lincoln <lincoln@lincolndeMacBook-Pro.local>
2016-10-22 21:20:07 +0800

    update

--- routes/message.js
+++ routes/message.js
@@ -2,18 +2,21 @@
  * Created by lincoln on 16/10/21.
  */
 var express = require('express');
-var router = express.Route();
+var router = express.Router();
 var DB = require('../models/db');
 var db = new DB();
 
 router.get('/message', function(req, res, next){
-    req.render('message', {'title': 'message'});
+    res.render('message', {'title': 'message'});
 });
 
-router.post('/message/list', function(req, res, next){
+router.get('/message/listMessage', function(req, res, next){
    var listSql = 'select * from f_message';
-   db.find(listSql, function(response){
-        req.render('message', {'title': 'list', 'messages': response.result})
+   db.find(listSql, function(result){
+        if(result.success){
+            res.render('listMessage', {'title': 'list', 'messages': result.data})
+        }
+
     })
 });
 router.post('/message/save', function(req, res, next){
@@ -23,9 +26,9 @@ router.post('/message/save', function(req, res, next){
     var create_time = new Date();
     values.push(data.mobile);
     values.push(data.content);
-    values.push(data.create_time);
-    db.insert(addSql, values, function(response){
-        req.json(response);
+    values.push(create_time);
+    db.insert(addSql, values, function(result){
+        res.json(result);
     })
 });
 router.post('/message/delete/:id', function(req, res, next){
@@ -33,8 +36,8 @@ router.post('/message/delete/:id', function(req, res, next){
     var values = [];
     values.push(id);
     var delSql = 'delete from f_message where id = ?';
-    db.delete(delSql, values, function(response){
-        req.json(response);
+    db.delete(delSql, values, function(result){
+        res.json(result);
     })
 });
 
@@ -43,8 +46,8 @@ router.post('/message/detail/:id', function(req, res, next){
     var values = [];
     var findSql = 'select * from f_message where id = ?';
     values.push(id);
-    db.findByCondition(findSql, values, function(response){
-        res.json(response);
+    db.findByCondition(findSql, values, function(result){
+        res.json(result);
     })
 });
 
